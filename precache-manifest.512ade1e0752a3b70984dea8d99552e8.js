f11acdfbfde52d594f6072aefdb377d0f7aa15d5
Admin <admin@MacBook-Pro-4.local>
2024-09-20 01:12:30 +0700

    Updates

---
+++ precache-manifest.512ade1e0752a3b70984dea8d99552e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1613621f35d9f9c424d75aa510100ca1",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ae886876793838ee76f1",
+    "url": "/my-app/static/css/main.08a37f44.chunk.css"
+  },
+  {
+    "revision": "f250f3150d7b98668b56",
+    "url": "/my-app/static/js/2.5a092441.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/my-app/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "ae886876793838ee76f1",
+    "url": "/my-app/static/js/main.10a8d941.chunk.js"
+  },
+  {
+    "revision": "09e0b948311a61c5c54a",
+    "url": "/my-app/static/js/runtime~main.a89e4939.js"
+  }
+]);
